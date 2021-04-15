b7b3de458ea48ac040cb54583a5e712f5cdae602
mi <1400823953@qq.com>
2021-04-15 15:00:23 +0800

    购物车1

--- src/views/shoppingCar.vue
+++ src/views/shoppingCar.vue
@@ -6,7 +6,14 @@
         <el-card>
             <div v-if="list.length==0"><img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farchive%2F22244b8e845a48a829ce7799a28eea3f201bfdbc.png&refer=http%3A%2F%2Fi0.hdslb.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1621045410&t=a8d6d213a538935a06f07315697da828" alt=""></div>
             <div class="car-item" v-for="(item, index) in list" :key="index">
-                <img src="item.imgUrl" alt="" />
+                <el-popover
+                placement="left"
+                width="400"
+                trigger="hover">
+                <img :src="item.imgUrl" alt=""/>
+                <img :src="item.imgUrl" slot="reference" alt="" style="width:40px;height:40px"/>
+                </el-popover>
+                
                 <p>{{ item.goodsName }}</p>
                 <p>数量：<el-input-number size="small" v-model="item.num" :min="1" :max="10" label="数量"></el-input-number></p>
                 <p style="color:red"><span>总价：¥ </span>{{ item.price }}*{{item.num}}={{(item.price*item.num).toString()+'.00'}}</p>
